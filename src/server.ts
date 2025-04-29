7701dc6cdb98918c39cf66d1caf67ea1a86212d3
rods <rods.desenvolvedor@gmail.com>
2025-04-29 11:20:52 -0400

    refactor: chamando primeira a conexão com o DB e dps iniciando o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
-import "dotenv/config"
+import "dotenv/config";
+import connectDatabase from "./configs/database";
 import app from "./app";
 
-const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("Servidor rodando na porta: ", PORT));
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+async function startServer() {
+    await connectDatabase(MONGODB_URI);
+
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
+
+startServer();
