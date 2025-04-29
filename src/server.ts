f1cf4bacb107f408088b5f6472da3d92a10c6ade
rods <rods.desenvolvedor@gmail.com>
2025-04-28 23:43:11 -0400

    chore: adicionado README inicial

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import app from "./app";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("Servidor rodando na porta: ", PORT))
+app.listen(PORT, () => console.log("Servidor rodando na porta: ", PORT));
