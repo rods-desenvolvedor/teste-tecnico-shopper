b7384363bd5672ce7a3a878baf92be367f7310ea
rods <rods.desenvolvedor@gmail.com>
2025-04-29 20:47:34 -0400

    test: teste manual do service, usando imagem local

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import callGeminiApi from "./services/geminiService";
+import path from "path";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.status(200).send("Servidor rodando");
+app.get("/", async (request, response) => {
+
+    const imagePath = path.resolve(__dirname, "../assets/test-image.jpg");
+
+    const aiResponse = await callGeminiApi(imagePath,
+        "Analisar a imagem com a medição do consumo de água ou gás e retornar esse valor"
+    );
+
+    response.status(200).send(aiResponse);
 })
 
 export default app;
