5311079b2be3cb260eafa683b94ded4a9d8a7cd5
rods <rods.desenvolvedor@gmail.com>
2025-05-01 13:43:08 -0400

    chamando o measureService diretamente em app (apenas para testes rapidos, refatorar para rota e controller depois)

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,47 @@
-import express from "express";
-import callGeminiApi from "./services/geminiService";
-import path from "path";
+    import express from "express";
+    import callGeminiApi from "./services/geminiService";
+    import path from "path";
+    import { Request, Response } from "express";
+    import uploadImage from "./services/measureService";
 
-const app = express();
+    const app = express();
 
-app.get("/", async (request, response) => {
+    app.use(express.json());
 
-    const imagePath = path.resolve(__dirname, "../assets/test-image.jpg");
+    app.get("/", async (request, response) => {
 
-    const aiResponse = await callGeminiApi(imagePath,
-        "Analisar a imagem com a medição do consumo de água ou gás e retornar esse valor"
-    );
+        const imagePath = path.resolve(__dirname, "../assets/test-image.jpg");
 
-    response.status(200).send(aiResponse);
-})
+        const aiResponse = await callGeminiApi(imagePath,
+            "Analisar a imagem com a medição do consumo de água ou gás e retornar esse valor"
+        );
 
-export default app;
+        response.status(200).send(aiResponse);
+    });
+
+    app.post("/upload", async (request : Request, response : Response) : Promise<void> => {
+
+        const {image, customer_code, measure_datetime, measure_type} = request.body;
+
+        console.log(request.body);
+
+        if(!image || !customer_code || !measure_datetime || !measure_type)
+        {
+            response.status(400).json(
+            {
+                "error_code" : "INVALID_DATA",
+                "error_description" : "Os dados fornecidos no corpo da requisição são inválidos."
+            });
+
+            return;
+        }
+
+        const geminiApiResponse = uploadImage(image, customer_code, measure_datetime, measure_type);
+
+        response.status(200).json(geminiApiResponse);
+
+    });
+
+    
+
+    export default app;
