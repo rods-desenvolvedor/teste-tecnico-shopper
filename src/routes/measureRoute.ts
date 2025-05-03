c65f486f2d2e1c3d1b373a6e2de2b0e16cde4521
rods <rods.desenvolvedor@gmail.com>
2025-05-03 14:56:27 -0400

    feat: adicionada a rota para confirmar a leitura

--- src/routes/measureRoute.ts
+++ src/routes/measureRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { uploadMeasure } from "../controllers/measureController";
+import { uploadMeasure, confirmMeasure } from "../controllers/measureController";
 
 const router = express.Router();
 
 router.post("/upload", uploadMeasure);
+router.patch("/confirm", confirmMeasure);
+
 
 
 export default router;
