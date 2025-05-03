020b50d369103f60156fab65c921d91086314148
rods <rods.desenvolvedor@gmail.com>
2025-05-03 17:19:49 -0400

    chore: ajuste para a api rodar na porta 80

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import connectDatabase from "./configs/database";
 import app from "./app";
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const MONGODB_URI = process.env.MONGODB_URI || "";
 
 async function startServer() {
