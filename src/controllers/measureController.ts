504dd5ca006b8abee6acc8ef012de70aea50caff
rods <rods.desenvolvedor@gmail.com>
2025-05-03 14:55:22 -0400

    feat: adicionado endpoint patch para confirmar a medição

--- src/controllers/measureController.ts
+++ src/controllers/measureController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import uploadImage from "../services/measureService";
-import { InvalidDataError, MeasureAlreadyExistsError } from "../errors/measureError";
+import { InvalidDataError, MeasureAlreadyExistsError, MeasureNotFoundError } from "../errors/measureError";
+import { confirmMeasureService } from "../services/measureService";
 
 export async function uploadMeasure(request: Request, response: Response): Promise<void> {
     const { image, customer_code, measure_datetime, measure_type } = request.body;
@@ -39,3 +40,49 @@ export async function uploadMeasure(request: Request, response: Response): Promi
         }
     }
 }
+
+
+export async function confirmMeasure(request: Request, response: Response): Promise<void>
+{
+    const { measure_uuid, confirmed_value } = request.body;
+
+    if (!measure_uuid ||typeof measure_uuid !== "string" || confirmed_value === undefined ||
+        typeof confirmed_value !== "number" ||
+        !Number.isInteger(confirmed_value)) 
+    {
+        response.status(400).json({
+            error_code: "INVALID_DATA",
+            error_description: "Os dados fornecidos no corpo da requisição são inválidos."
+        });
+    }
+
+    try {
+        await confirmMeasureService(measure_uuid, confirmed_value);
+
+        response.status(200).json({ success: true });
+    } catch (error) {
+        if (error instanceof InvalidDataError) {
+            response.status(400).json({
+                error_code: "INVALID_DATA",
+                error_description: error.message
+            });
+        }
+
+        if (error instanceof MeasureNotFoundError) {
+            response.status(404).json({
+                error_code: "MEASURE_NOT_FOUND",
+                error_description: "Leitura não encontrada."
+            });
+        }
+
+        console.error("Erro no confirmMeasure:", error);
+        
+        response.status(500).json({
+            error_code: "CONFIRM_ERROR",
+            error_description: "Erro inesperado ao processar a confirmação."
+        });
+    }
+};
+
+
+
