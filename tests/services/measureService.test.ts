8ff4066288b8758122dc3945081dcc956f44b8b2
rods <rods.desenvolvedor@gmail.com>
2025-05-03 00:28:51 -0400

    fix: feito as correções no teste unitario do measureService para utilizar mock do model

--- tests/services/measureService.test.ts
+++ tests/services/measureService.test.ts
@@ -2,11 +2,16 @@ import * as geminiService from "../../src/services/geminiService";
 import uploadImage from "../../src/services/measureService";
 import { UploadImageDTO } from "../../src/dtos/uploadImageDTO";
 import fs from "fs";
+import MeasureModel from "../../src/Models/Measure";
+import { InvalidDataError, MeasureAlreadyExistsError } from "../../src/errors/measureError";
 
-// Mock do módulo geminiService
+
 jest.mock("../../src/services/geminiService", () => ({
     callGeminiApi: jest.fn(),
 }));
+
+
+jest.mock("../../src/Models/Measure");
 
 describe("uploadImage", () => {
 
@@ -24,8 +29,10 @@ describe("uploadImage", () => {
         jest.clearAllMocks();
     });
 
-    it("deve salvar a imagem e retornar a resposta da API Gemini", async () => {
+    it("deve salvar a imagem, persistir a medição no banco e retornar os dados corretos", async () => {
         (geminiService.callGeminiApi as jest.Mock).mockResolvedValue(fakeGeminiResponse);
+        (MeasureModel.findOne as jest.Mock).mockResolvedValue(null);  
+        (MeasureModel.create as jest.Mock).mockResolvedValue({});   
 
         const result = await uploadImage(uploadDTO);
 
@@ -33,18 +40,19 @@ describe("uploadImage", () => {
         expect(result).toHaveProperty("measure_value", 123);
         expect(result).toHaveProperty("measure_uuid");
 
-        expect(geminiService.callGeminiApi).toHaveBeenCalledTimes(1);
+        expect(MeasureModel.create).toHaveBeenCalled();
     });
 
     it("deve lançar erro se a imagem base64 não for fornecida", async () => {
         const invalidDTO = { ...uploadDTO, base64Image: "" };
 
         await expect(uploadImage(invalidDTO))
-            .rejects.toThrow("Imagem em base64 inválida.");
+            .rejects.toThrow(InvalidDataError);
     });
 
     it("deve lançar erro se ocorrer falha ao salvar o arquivo temporário", async () => {
         jest.spyOn(fs.promises, "writeFile").mockRejectedValueOnce(new Error("Falha ao salvar arquivo"));
+        (MeasureModel.findOne as jest.Mock).mockResolvedValue(null);  
 
         await expect(uploadImage(uploadDTO))
             .rejects.toThrow("Falha ao processar a imagem ou chamar a API do Gemini.");
@@ -52,8 +60,17 @@ describe("uploadImage", () => {
 
     it("deve lançar erro se a API Gemini falhar", async () => {
         (geminiService.callGeminiApi as jest.Mock).mockRejectedValue(new Error("Falha na Gemini"));
+        (MeasureModel.findOne as jest.Mock).mockResolvedValue(null); 
 
         await expect(uploadImage(uploadDTO))
             .rejects.toThrow("Falha ao processar a imagem ou chamar a API do Gemini.");
+    });
+
+    it("deve lançar erro se já existir uma medição para o mesmo mês e tipo", async () => {
+        (geminiService.callGeminiApi as jest.Mock).mockResolvedValue(fakeGeminiResponse); 
+        (MeasureModel.findOne as jest.Mock).mockResolvedValueOnce({ id: "existing-id" }); 
+    
+        await expect(uploadImage(uploadDTO))
+            .rejects.toThrow(MeasureAlreadyExistsError);
     });
 });
